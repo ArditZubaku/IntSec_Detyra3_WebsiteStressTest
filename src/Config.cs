72c6b0cf7118bddb642edea11840273cbe078a29
Drilonii <58754697+Drilonii@users.noreply.github.com>
2023-01-08 20:00:37 +0100

    Update Config.cs

--- src/Config.cs
+++ src/Config.cs
@@ -1,1 +1,34 @@
+using System.Collections.Generic;
 
+namespace Stressi
+{
+    public class Config
+    {
+    
+        public string Url { get; set; }
+
+        public string HttpMethod { get; set; }
+
+        public long? ConcurrentUsers { get; set; }
+
+        public long? Repetitions { get; set; }
+
+        public bool Verbose { get; set; }
+
+        public string UserAgent { get; set; }
+
+        public Dictionary<string, string> Headers { get; set; }
+
+        public int? Timeout { get; set; }
+    }
+
+    public class DefaultConfig
+    {
+
+        public const string HttpMethod = "GET";
+
+        public const int ConcurrentUsers = 10;
+
+        public const int Repetitions = 10;
+    }
+}
