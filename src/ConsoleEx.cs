01655af64534cbcdc1551b79956feb793a676018
arianaberisha <94613006+ArianaBrsh@users.noreply.github.com>
2023-01-08 19:58:28 +0100

    Update ConsoleEx.cs

--- src/ConsoleEx.cs
+++ src/ConsoleEx.cs
@@ -58,3 +58,70 @@ namespace Stressi
                 .Where(kv => kv.Length == 2)
                 .ToDictionary(kv => kv[0], kv => kv[1]);
         }
+
+        public static int? GetArgValueAsInt32(params string[] keys)
+        {
+            var value = GetArgValue(keys);
+
+            if (value != null &&
+                int.TryParse(value, out var temp))
+            {
+                return temp;
+            }
+
+            return null;
+        }
+
+        public static bool IsSwitchPresent(params string[] keys)
+        {
+            if (CmdArgs == null)
+            {
+                return false;
+            }
+
+            return keys.Any(key => CmdArgs.Any(n => n == $"{ShortHandOptionPrefix}{key}") ||
+                                   CmdArgs.Any(n => n == $"{LongHandOptionPrefix}{key}"));
+        }
+
+        public static bool NoOptions()
+        {
+            return CmdArgs?.Length == 0;
+        }
+
+        #endregion
+
+        #region Write functions
+
+        public static void WriteError(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write("[ERROR] ");
+
+            Console.ResetColor();
+            Console.WriteLine(message);
+        }
+
+        public static void WriteLineWordWrapped(string message)
+        {
+            var words = message.Split(' ');
+            var charsLeft = Console.WindowWidth;
+
+            foreach (var word in words)
+            {
+                if (word.Length > charsLeft)
+                {
+                    charsLeft = Console.WindowWidth;
+                    Console.WriteLine();
+                }
+
+                charsLeft -= word.Length + 1;
+
+                Console.Write($"{word} ");
+            }
+
+            Console.WriteLine();
+        }
+
+        #endregion
+    }
+}
